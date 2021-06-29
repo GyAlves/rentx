302794a0ae790aaff20a937d7e777a7f2d0a5ee3
GyAlves <gyasminassuncao@uni9.edu.br>
2021-06-29 16:53:20 -0300

    Creating app container - rentx

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,7 @@
 import express from 'express';
-import routes from './routes';
 
 const app = express();
 
 app.listen(3333, () => {
-    console.log('Server running on port 3333 ! 🚀 ');
+  console.log('Server running on port 3333 !! 🚀 ');
 });

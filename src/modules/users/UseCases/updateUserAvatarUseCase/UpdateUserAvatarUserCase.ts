86d0f5b9d5652e99bbd4c4fe9423e73e3cc9cf5e
GyAlves <gyasminassuncao@uni9.edu.br>
2021-07-02 22:53:13 -0300

    Update user avatar  feature finished

---
+++ src/modules/users/UseCases/updateUserAvatarUseCase/UpdateUserAvatarUserCase.ts
@@ -0,0 +1,23 @@
+import { IUsersRepository } from "@modules/users/repositories/IUsersRepository";
+import { deleteFile } from "@utils/file";
+import { inject, injectable } from "tsyringe";
+
+@injectable()
+class UpdateUserAvatarUseCase {
+    constructor(
+        @inject('UsersRepository') private usersRepository: IUsersRepository,
+    ) { }
+    async execute(user_id: string, avatar_file: string): Promise<void> {
+        const user = await this.usersRepository.findById(user_id);
+
+        if (user.avatar) {
+            await deleteFile(`./tmp/avatar/${user.avatar}`);
+        }
+
+        user.avatar = avatar_file;
+
+        await this.usersRepository.create(user);
+    }
+}
+
+export { UpdateUserAvatarUseCase }
--- src/shared/infra/http/routes/users.routes.ts
+++ src/shared/infra/http/routes/users.routes.ts
@@ -1,9 +1,12 @@
+import upload from "@config/upload";
 import { CreateUserController } from "@modules/users/UseCases/createUsers/CreateUserController";
 import { DeleteUserByIdController } from "@modules/users/UseCases/deleteUserByID/DeleteUserByIdController";
 import { ListUserByIdController } from "@modules/users/UseCases/listUserByID/ListUserByIdController";
 import { ListUsersController } from "@modules/users/UseCases/listUsers/ListUsersController";
+import { UpdateUserAvatarController } from "@modules/users/UseCases/updateUserAvatarUseCase/UpdateUserAvatarController";
 import { UpdateUserController } from "@modules/users/UseCases/updateUsers/UpdateUserController";
 import { Router } from "express";
+import multer from "multer";
 
 const usersRouter = Router();
 
@@ -12,11 +15,16 @@ const listUsersController = new ListUsersController();
 const listUserByIdController = new ListUserByIdController()
 const updateUserController = new UpdateUserController();
 const deleteUseByIdController = new DeleteUserByIdController();
+const updateUserAvatarController = new UpdateUserAvatarController();
+
+
+const uploadAvatar = multer(upload.upload("./tmp/avatar"))
 
 usersRouter.post("", createUserController.handle);
 usersRouter.get("", listUsersController.handle);
 usersRouter.get("/:user_id", listUserByIdController.handle);
 usersRouter.put("/:user_id/update", updateUserController.handle);
 usersRouter.delete("/:user_id", deleteUseByIdController.handle);
+usersRouter.patch("/:user_id/avatar", uploadAvatar.single("avatar"), updateUserAvatarController.handle);
 
 export { usersRouter }
